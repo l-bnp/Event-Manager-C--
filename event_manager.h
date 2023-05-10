5501afd3fb0813cf535edab6e5acede64b43b670
Bruno Lima <npl.bruno@gmail.com>
2023-05-09 23:45:53 -0300

    Added singleton support. Added off() method. Added mutex.

--- event_manager.h
+++ event_manager.h
@@ -15,7 +15,9 @@
 //     event_manager.emitEvent(commandJson.at("command_type").get<std::string>(), commandJson.at("channel_type").get<std::string>(),
 //                                            commandJson.at("channel_number").get<unsigned int>(), commandJson.at("volume_db").get<int>());
 //
-// This is a minimal implementation. the usual off() method to unregister functions can be easily implemented.
+//  The on() method returns a size_t value that can be used to unregister the function from the event using the off() method like this:
+//      size_t function_id = event_manager.on<int>("my_event", [](int value) { std::cout << "Received value: " << value << std::endl; });
+//      event_manager.off("my_event", function_id);
 
 #ifndef EVENT_MANAGER_H
 #define EVENT_MANAGER_H
@@ -25,14 +27,19 @@
 #include <map>
 #include <memory>
 #include <string>
+#include <mutex>
 
 // Define a template alias for std::function with variadic template arguments.
 template <typename... Args>
 using FunctionType = std::function<void(Args...)>;
 
-// Define a template alias for a vector of FunctionType with variadic template arguments.
+// Define a template alias for a pair of a unique identifier (size_t) and a FunctionType.
 template <typename... Args>
-using FunctionVector = std::vector<FunctionType<Args...>>;
+using FunctionIdPair = std::pair<size_t, FunctionType<Args...>>;
+
+// Define a template alias for a vector of FunctionIdPair with variadic template arguments.
+template <typename... Args>
+using FunctionVector = std::vector<FunctionIdPair<Args...>>;
 
 // Base class for a vector of functions. It will be inherited by DerivedFunctionVector.
 struct BaseFunctionVector
@@ -50,37 +57,77 @@ struct DerivedFunctionVector : public BaseFunctionVector
 class EventManager
 {
 public:
+    // Static method to access the Singleton instance.
+    static EventManager &getInstance()
+    {
+        static EventManager instance;
+        return instance;
+    }
+
     // Register a function or lambda function with a specific event name.
     template <typename... Args, typename F>
-    void on(std::string eventName, F &&newFunc);
+    size_t on(std::string eventName, F &&newFunc);
+
+    template <typename... Args>
+    void off(std::string eventName, size_t id);
 
     // Emit an event with the specified name and pass arguments to the registered functions.
     template <typename... Args>
     void emitEvent(std::string eventName, Args... args);
 
 private:
+    // Private constructor.
+    EventManager() = default;
+
+    // Delete copy constructor and copy assignment operator.
+    EventManager(const EventManager &) = delete;
+    EventManager &operator=(const EventManager &) = delete;
     // A map that associates event names with their respective function vectors.
     std::map<std::string, std::shared_ptr<BaseFunctionVector>> functionsMap;
+    std::mutex functionsMapMutex;
 };
 
 // Register a function or lambda function with a specific event name.
 template <typename... Args, typename F>
-void EventManager::on(std::string eventName, F &&newFunc)
+size_t EventManager::on(std::string eventName, F &&newFunc)
 {
+    std::lock_guard<std::mutex> lock(functionsMapMutex);
     FunctionType<Args...> func = newFunc;
     auto itr = functionsMap.find(eventName);
+    size_t id = 0;
 
     // If the event name already exists, add the new function to the existing vector.
     if (itr != functionsMap.end())
     {
-        static_cast<DerivedFunctionVector<Args...> *>(itr->second.get())->functions.push_back(func);
+        auto &functions = static_cast<DerivedFunctionVector<Args...> *>(itr->second.get())->functions;
+        id = functions.size();
+        functions.emplace_back(id, func);
     }
     // If the event name does not exist, create a new vector and add the function to it.
     else
     {
         auto functionVector = std::make_shared<DerivedFunctionVector<Args...>>();
-        functionVector->functions.push_back(func);
+        functionVector->functions.emplace_back(id, func);
         functionsMap.insert({eventName, functionVector});
+    }
+
+    return id;
+}
+
+// Method to remove a function with a specific event name and function ID.
+template <typename... Args>
+void EventManager::off(std::string eventName, size_t id)
+{
+    std::lock_guard<std::mutex> lock(functionsMapMutex);
+    auto itr = functionsMap.find(eventName);
+
+    if (itr != functionsMap.end())
+    {
+        auto &functions = static_cast<DerivedFunctionVector<Args...> *>(itr->second.get())->functions;
+        functions.erase(std::remove_if(functions.begin(), functions.end(),
+                                       [&](const FunctionIdPair<Args...> &pair)
+                                       { return pair.first == id; }),
+                        functions.end());
     }
 }
 
@@ -88,15 +135,16 @@ void EventManager::on(std::string eventName, F &&newFunc)
 template <typename... Args>
 void EventManager::emitEvent(std::string eventName, Args... args)
 {
+    std::lock_guard<std::mutex> lock(functionsMapMutex);
     auto itr = functionsMap.find(eventName);
 
     // If the event name exists, execute all registered functions with the provided arguments.
     if (itr != functionsMap.end())
     {
         auto &functions = static_cast<DerivedFunctionVector<Args...> *>(itr->second.get())->functions;
-        for (auto &func : functions)
+        for (auto &funcPair : functions)
         {
-            func(args...);
+            funcPair.second(args...);
         }
     }
 }
